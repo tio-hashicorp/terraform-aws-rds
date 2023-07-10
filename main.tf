ed2e3889f53ad37dd0af3393461f2123adc580b1
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2023-07-10 11:26:20 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_db_instance" "education" {
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
-  engine_version         = "14.1"
+  engine_version         = "14.2"
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.education.name
